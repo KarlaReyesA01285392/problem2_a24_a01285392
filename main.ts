307f309f4d31be8e83aaebc3f37e9309a1d44d41
KarlaReyesA01285392 <70342598+KarlaReyesA01285392@users.noreply.github.com>
2020-10-11 13:31:37 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    for (let Y = 0; Y <= 0; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plot(X, Y)
+            basic.pause(100)
+        }
+    }
+})
+let X = 0
+let Y = 0
+basic.forever(function () {
+	
+})
